5cfffbc9cb0f341b8da60155906ee7108d169df6
Masayukiii <szkmsyk041792@gmail.com>
2019-06-22 10:28:28 +0900

    add utility

---
+++ src/utility.lisp
@@ -0,0 +1,44 @@
+(defpackage mgr.utility
+  (:use :cl
+	:ppcre)
+  (:export :create
+	   :get-migration-name))
+(in-package :mgr.utility)
+
+; TODO: use current time instead of universal time.
+(defun create (name)
+  (let* (
+	(timestamp (write-to-string (get-universal-time)))
+	 (filename (concatenate 'string timestamp "_" name ".lisp"))
+	)
+    (with-open-file (migration-file (merge-pathnames filename)
+                     :direction :output
+                     :if-exists :supersede
+                     :if-does-not-exist :create)
+  (format migration-file (default-migration-content filename)))
+    
+    ))
+
+(defun default-migration-content (filename)  
+(format nil "
+(in-package :cl-user)
+(defpackage :~a
+  (:use :cl
+        :mgr)
+)
+(in-package :~a)
+
+(defup
+   (create-table \"users\")
+   (add-column \"users\" \"name\" \"varchar(255)\")
+ )
+
+(defdown
+    (drop-table \"users\")
+)" filename filename))
+
+
+(defun get-migration-name (path)
+   (svref (nth-value 1 (ppcre:scan-to-strings "/([^\/.]+).lisp" path)) 0)
+  )
+
